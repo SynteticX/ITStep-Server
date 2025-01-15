09ba6a097529a5ab8a27aa8a525ef664949330a2
SynteticX <rockislie2@gmail.com>
2025-01-15 19:49:22 +0500

    Запросы для авторизации

--- Social-Network-Client/src/App.vue
+++ Social-Network-Client/src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
+  import mainStore from "./store/store";
+
+  const store = mainStore();
+
+  const handleClick = async () => {
+    await store.handleAuth("Admin", "password");
+  }
 </script>
 
 <template>
   <div>
-    
+    <button @click="handleClick">Login</button>
   </div>
 </template>
--- Social-Network-Client/src/store/store.js
+++ Social-Network-Client/src/store/store.js
@@ -1,5 +1,53 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useMainStore = defineStore('main', () => {
+const useMainStore = defineStore('main', () => {
+    const token = ref("");
+    const user = ref({});
 
+    const API = "http://localhost:3000/"
+
+    const handleAuth = async (login, password) => {
+
+        console.log("Auth login: " + login)
+        console.log("Auth pass: " + password)
+
+        const query = {
+            login: login,
+            password: password
+        }
+
+        const response = await fetch(API + "auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(query)
+        })
+        // .then(data => data.json())
+        .catch(err => console.log("Ошибка при запросе: " + err));
+
+        console.log(response);
+
+        if (!response.ok) {
+            console.log("Ошибка при запросе!")
+        }
+        const data = await response.json();
+
+        if (response.status && response.status == 200) {
+            console.log(data);
+        }
+        if (response.status && response.status == 400) {
+            console.log(data);
+        }
+        if (response.status && response.status == 500) {
+            console.log(data);
+        }
+
+
+    }
+
+    return { token, user, handleAuth };
 });
+
+export default useMainStore;
