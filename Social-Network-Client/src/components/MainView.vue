c6ba2856ecb2613092bef76215f854ee1c4e4ebb
SynteticX <rockislie2@gmail.com>
2025-01-20 21:12:40 +0500

    Post autoupdate

--- Social-Network-Client/src/components/MainView.vue
+++ Social-Network-Client/src/components/MainView.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <button @click="handleClick()">CLICK</button>
+        <button @click="handleClick()">Update</button>
 
         <div v-for="(post) in posts">
             <div>
+                Автор: {{ post.author }}
+                Дата: {{ getDate(post.datetime) }}
                 {{ post.text }}
             </div>
         </div>
@@ -11,17 +13,28 @@
 </template>
 
 <script setup>
-    import { ref, watch } from 'vue';
+    import { ref, watch, onMounted } from 'vue';
     import usePostStore from '../store/postStore';
     const postStore = usePostStore();
 
-    const posts = ref(postStore.posts);
-    watch(postStore.posts, (newValue, oldValue) => {
-        console.log(postStore.posts);
-        posts.value = postStore.posts;
+    onMounted(async () => {
+        await postStore.getAllPosts();
     });
+
+    const posts = ref(postStore.posts);
+    watch(postStore, (newValue, oldValue) => {
+        posts.value = newValue.posts;
+    }, {deep: true});
 
     const handleClick = async () => {
         await postStore.getAllPosts();
+    }
+
+    const getDate = (rawDate) => {
+        console.log(rawDate)
+        if (rawDate) {
+            const date = new Date(Date.parse(rawDate));
+            return date.toDateString();
+        }
     }
 </script>
